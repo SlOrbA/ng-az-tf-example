014238aee5a120e67dbe1dd88506b53f80690fa0
Lari Korpi <lari.korpi@tieto.com>
2020-09-15 13:04:05 +0300

    upgrade to support new terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
 # Create a resource group for the Angular app
 resource "azurerm_resource_group" "example" {
   name     = "example"
@@ -12,8 +16,8 @@ resource "random_string" "example" {
 
 resource "azurerm_storage_account" "example" {
   name                     = "example${random_string.example.result}"
-  location                 = "${azurerm_resource_group.example.location}"
-  resource_group_name      = "${azurerm_resource_group.example.name}"
+  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.example.name
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -25,37 +29,37 @@ resource "azurerm_storage_account" "example" {
 
 #resource "azurerm_storage_container" "web" {
 #  name                     = "$web"
-#  storage_account_name     = "${azurerm_storage_account.example.name}"
+#  storage_account_name     = azurerm_storage_account.example.name
 #}
 
 resource "null_resource" "example" {
   provisioner "local-exec" {
     command     = "az storage blob upload-batch --destination '$web' --source ../example-app/dist/example-app"
     environment = {
-      AZURE_STORAGE_ACCOUNT = "${azurerm_storage_account.example.name}"
-      AZURE_STORAGE_KEY     = "${azurerm_storage_account.example.primary_access_key}"
+      AZURE_STORAGE_ACCOUNT = azurerm_storage_account.example.name
+      AZURE_STORAGE_KEY     = azurerm_storage_account.example.primary_access_key
     }
   }
-  depends_on = ["azurerm_storage_account.example"]
+  depends_on = [azurerm_storage_account.example]
 }
 
 resource "azurerm_cdn_profile" "example" {
   name                = "example-cdn"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   sku                 = "Standard_Microsoft"
 }
 
 resource "azurerm_cdn_endpoint" "example" {
   name                = "example-cdn-${random_string.example.result}" 
-  profile_name        = "${azurerm_cdn_profile.example.name}"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  origin_host_header  = "${basename(azurerm_storage_account.example.primary_web_endpoint)}"
+  profile_name        = azurerm_cdn_profile.example.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  origin_host_header  = basename(azurerm_storage_account.example.primary_web_endpoint)
 
   origin {
-    name      = "${azurerm_storage_account.example.name}"
-    host_name = "${basename(azurerm_storage_account.example.primary_web_endpoint)}"
+    name      = azurerm_storage_account.example.name
+    host_name = basename(azurerm_storage_account.example.primary_web_endpoint)
   }
 }
 
